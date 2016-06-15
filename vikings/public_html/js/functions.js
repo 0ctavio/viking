a2c2a0fa52bb0d0cf2b8e2358968d854c9b0b9b7
Octavio <Octavio@192.168.42.112>
2016-06-15 13:05:46 +0200

    Pruebas iniciales

--- vikings/public_html/js/functions.js
+++ vikings/public_html/js/functions.js
@@ -1,4 +1,4 @@
-var vikings = [];
+var pit = [];
 
 var Viking = function (name, health, strength) {
     this.name = name,
@@ -6,3 +6,56 @@ var Viking = function (name, health, strength) {
     this.strength = strength;
 };
 
+function getDamage(){
+    return Math.floor((Math.random() * 20) + 20);
+}
+
+function addVikingToPit(name, health, strength) {
+    var vinking = new Viking(name, health, strength);
+    
+}
+
+
+/*
+function getDamage() {
+    var pointsDamage = Math.floor((Math.random() * 20) + 20);
+    function checkDamage() {
+        return pointsDamage;
+    }
+    return checkDamage;
+}
+
+var randomDamage = getDamage();
+*/
+
+
+
+function getDamage() {
+    var pointsDamage = Math.floor((Math.random() * 20) + 20);
+    function checkDamage() {
+        return pointsDamage;
+    }
+    return pointsDamage;
+}
+
+var randomDamage = getDamage();
+
+//------------------
+function getDamage() {
+    return function (){
+        Math.floor((Math.random() * 20) + 20);
+    };
+}
+
+var randomDamage = getDamage();
+
+//---------------
+
+function getDamage() {
+    return function (){
+var x = Math.floor((Math.random() * 20) + 20);
+        
+    };
+}
+
+var randomDamage = getDamage();
