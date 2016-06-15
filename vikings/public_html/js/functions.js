f54ee6b9f48630f923290e18f2b39c0bc3d8ff96
Octavio <Octavio@192.168.42.112>
2016-06-15 13:47:02 +0200

    Correccion objetos viking

--- vikings/public_html/js/functions.js
+++ vikings/public_html/js/functions.js
@@ -1,35 +1,59 @@
-var pit = [];
-
+//este es el constructor
 var Viking = function (name, health, strength) {
     this.name = name,
     this.health = health,
     this.strength = strength;
 };
 
+
+//esta es la arena
+var pit = function ( ) {
+    this.fighters = [ ],
+    this.numbersFighters = 0;
+};
+
+
+//añadir vikingos a la arena
+pit.prototype.addVikingToPit = function (viking) {
+    this.fighters.push (viking);
+    //pit.push = ();
+};
+
+var viking1 = new Viking ("La montaña", 100, 25);
+var viking2 = new Viking ("Thor", 100, 30);
+
+pit.addVikingToPit(viking1);
+pit.addVikingToPit(viking2);
+
+//esta es la función que obtiene daño aleatorio
 function getDamage(){
     return Math.floor((Math.random() * 20) + 20);
-}
+};
 
-function addVikingToPit(name, health, strength) {
-    var vinking = new Viking(name, health, strength);
-    
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 /*
-function getDamage() {
-    var pointsDamage = Math.floor((Math.random() * 20) + 20);
-    function checkDamage() {
-        return pointsDamage;
-    }
-    return checkDamage;
-}
-
-var randomDamage = getDamage();
-*/
-
-
-
 function getDamage() {
     var pointsDamage = Math.floor((Math.random() * 20) + 20);
     function checkDamage() {
@@ -40,22 +64,4 @@ function getDamage() {
 
 var randomDamage = getDamage();
 
-//------------------
-function getDamage() {
-    return function (){
-        Math.floor((Math.random() * 20) + 20);
-    };
-}
-
-var randomDamage = getDamage();
-
-//---------------
-
-function getDamage() {
-    return function (){
-var x = Math.floor((Math.random() * 20) + 20);
-        
-    };
-}
-
-var randomDamage = getDamage();
+*/
